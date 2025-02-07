66bac2cbed81e36974b086e502783bc05178769d
SujalBhatt <vimlabhatt97@gmail.com>
2025-02-07 11:38:48 +0530

    comitted changes

---
+++ Frontend/src/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Sujal Bhatt</h1>
+      <p>Email: abc123@example.com</p>
+      <p>Phone:  123-456-7890</p>
+    </div>
+  )
+}
+
+export default Home
